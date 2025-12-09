dd714a313ed7cda2d195020d94bf3cc2c924b6e6
Nanda Safiq Alfiansyah <nandasafiqalfiansyah@gmail.com>
2025-12-09 16:09:41 +0700

    [feat] add wacv data

--- Feature_extract/Extract_MediaPipe_features.py
+++ Feature_extract/Extract_MediaPipe_features.py
@@ -10,14 +10,14 @@ import matplotlib.pyplot as plt
 import mediaPipeFeatureExtractor as fmp
 
 #path to WACV image folders
-path1 = "/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/dataset/1"
-path2 = "/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/dataset/2"
-path3 = "/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/dataset/3"
+path1 = "../WACV data/dataset/1"
+path2 = "../WACV data/dataset/2"
+path3 = "../WACV data/dataset/3"
 
 
-df_OF0 = pd.read_csv("/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/processedData0.csv")
-df_OF1 = pd.read_csv("/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/processedData1.csv")
-df_OF2 = pd.read_csv("/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/processedData2.csv")
+df_OF0 = pd.read_csv("../WACV data/processedData0.csv")
+df_OF1 = pd.read_csv("../WACV data/processedData1.csv")
+df_OF2 = pd.read_csv("../WACV data/processedData2.csv")
 
 lm_dic0 = fmp.faceMesh_extract(path1,False)
 lm_dic1 = fmp.faceMesh_extract(path2,False)
@@ -28,6 +28,6 @@ df_merge1 = fmp.buildFeatureDataframe(lm_dic1,1,df_OF1)
 df_merge2 = fmp.buildFeatureDataframe(lm_dic2,2,df_OF2)
 
 
-df_merge0.to_csv("/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/merged_data0.csv")
-df_merge1.to_csv("/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/merged_data1.csv")
-df_merge2.to_csv("/Users/rijju/Documents/GitHub/Student-Engagement/WACV data/merged_data2.csv")
+df_merge0.to_csv("../WACV data/merged_data0.csv")
+df_merge1.to_csv("../WACV data/merged_data1.csv")
+df_merge2.to_csv("../WACV data/merged_data2.csv")
